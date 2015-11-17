070619154ec6d4b1e3884f4c2d93908a272ac604
Iwakura Taro <taro@mail333.com>
2015-11-17 15:57:07 +0200

    Updating to version 6.1.

--- config.h
+++ config.h
@@ -1,6 +1,10 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
+static const char *fonts[] = {
+       "monospace:size=10"
+};
+static const char dmenufont[]       = "monospace:size=10";
 static const char font[]            = "-misc-*-medium-r-normal-*-14-*-*-*-*-*-*-*";
 static const char normbordercolor[] = "#999999";
 static const char normbgcolor[]     = "#000066";
@@ -10,13 +14,18 @@ static const char selbgcolor[]      = "#ccccff";
 static const char selfgcolor[]      = "#000066";
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const Bool showbar           = False;    /* False means no bar */
-static const Bool topbar            = True;     /* False means bottom bar */
+static const int showbar            = 0;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+
 
 /* tagging */
 static const char *tags[] = { "ichi", "ni", "san", "yon", "go", "roku", "nana", "hachi", "kyuu" };
 
 static const Rule rules[] = {
+       /* xprop(1):
+        *      WM_CLASS(STRING) = instance, class
+        *      WM_NAME(STRING) = title
+        */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
 	{ "Firefox",  NULL,       NULL,       1,            False,       -1 },
@@ -28,9 +37,9 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster      = 1;    /* number of clients in master area */
-static const Bool resizehints = False; /* False means respect size hints in tiled resizals */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -51,9 +60,10 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *browsercmd[]  = { "opera", NULL };
-static const char *utermtmuxcmd[]  = { "uxterm", "-si", "-vb", "+sb", "-fullscreen", "-C", "-e", "tmux new-session -d -s hajime \"tmux source ~/.tmux.conf.$HOST\"; exec tmux attach", NULL };
+static const char *utermtmuxcmd[]  = { "uxterm", "-si", "-vb", "+sb", "-fullscreen", "-C", "-e", "tmux new-session -d -s hajime; tmux source ~/.tmux.conf.$HOST; exec tmux attach", NULL };
 static const char *utermtmuxattachcmd[]  = { "uxterm", "-si", "-vb", "+sb", "-fullscreen", "-C", "-e", "exec tmux -2u attach", NULL };
 static const char *xtrlockcmd[]  = { "xtrlock", NULL };
 static const char *viewercmd[]  = { "evince", NULL };
@@ -111,8 +121,7 @@ static Key keys[] = {
 };
 
 /* button definitions */
-/* click can be a tag number (starting at 0),
- * ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+/* click can be ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
